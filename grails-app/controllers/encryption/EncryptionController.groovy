607ce4f16a7a59520a0b1f45ec0f4ca16a3e7bb2
anijor <anijorshrestha@gmail.com>
2016-10-29 13:54:25 +0545

    Each RGB value

--- grails-app/controllers/encryption/EncryptionController.groovy
+++ grails-app/controllers/encryption/EncryptionController.groovy
@@ -4,6 +4,7 @@ import grails.util.Holders
 import org.apache.commons.io.FilenameUtils
 
 import javax.imageio.ImageIO
+import java.awt.Color
 import java.awt.image.BufferedImage
 import java.awt.image.DataBufferByte
 import java.awt.image.Raster
@@ -59,38 +60,62 @@ class EncryptionController {
             BufferedImage image = ImageIO.read(new File("D:\\ImageEncryptionAndDecryption\\web-app\\photo.PNG"));
             int height= image.getHeight();
             int width= image.getWidth();
+            int pixel;
+            int k=1;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    System.out.println("x,y: " + j + ", " + i);
+                    pixel = image.getRGB(j, i);
+                    k++;
+                    println(k)
 
-            Raster raster= image.getData();
-            int[][] pixels=new int[width][height];
-            for (int x=0;x<width;x++)
-            {
-                for(int y=0;y<height;y++)
-                {
-                    pixels[x][y]=raster.getSample(x,y,0);
-
-
+                    System.out.println("");
+                    int alpha = (pixel >> 24) & 0xff;
+                    int red = (pixel >> 16) & 0xff;
+                    int green = (pixel >> 8) & 0xff;
+                    int blue = (pixel) & 0xff;
+                    println(alpha)
+                    println(red)
+                    println(green)
+                    println(blue)
+                    System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
                 }
             }
-            print(pixels)
 
-          //  return pixels;
+
+
+//            Raster raster= image.getData();
+//            int[][] pixels=new int[width][height];
+//            for (int x=0;x<width;x++)
+//            {
+//                for(int y=0;y<height;y++)
+//                {
+//                    pixels[x][y]=raster.getSample(x,y,0);
+//
+//
+//                }
+//            }
+//            print(pixels)
+//
+//
+//            return pixels;
 
 
           //  write it to byte array in-memory (jpg format)
-            ByteArrayOutputStream b = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", b);
-
-            // do whatever with the array...
-            byte[] jpgByteArray = b.toByteArray();
-            println(jpgByteArray)
-            // convert it to a String with 0s and 1s
-            StringBuilder sb = new StringBuilder();
-
-            for (byte by : jpgByteArray)
-                sb.append(Integer.toBinaryString(by & 0xFF));
-
-
-            System.out.println(sb.toString());
+//            ByteArrayOutputStream b = new ByteArrayOutputStream();
+//            ImageIO.write(image, "png", b);
+//
+//            // do whatever with the array...
+//            byte[] jpgByteArray = b.toByteArray();
+//            println(jpgByteArray)
+//            // convert it to a String with 0s and 1s
+//            StringBuilder sb = new StringBuilder();
+//
+//            for (byte by : jpgByteArray)
+//                sb.append(Integer.toBinaryString(by & 0xFF));
+//
+//
+//            System.out.println(sb.toString());
 
 
 
