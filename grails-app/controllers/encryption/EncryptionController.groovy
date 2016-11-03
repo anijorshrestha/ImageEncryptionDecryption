64f0eca8cf7e890a45f757b97053ccb3f4077ca9
anijor <anijorshrestha@gmail.com>
2016-11-03 21:10:20 +0545

    xor

--- grails-app/controllers/encryption/EncryptionController.groovy
+++ grails-app/controllers/encryption/EncryptionController.groovy
@@ -61,47 +61,48 @@ class EncryptionController {
             int height= image.getHeight();
             int width= image.getWidth();
             int pixel;
-            int k=1;
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    System.out.println("x,y: " + j + ", " + i);
-                    pixel = image.getRGB(j, i);
-                    k++;
-                    println(k)
-
-                    System.out.println("");
-                    int alpha = (pixel >> 24) & 0xff;
-                    int red = (pixel >> 16) & 0xff;
-                    int green = (pixel >> 8) & 0xff;
-                    int blue = (pixel) & 0xff;
-                    println(alpha)
-                    println(red)
-                    println(green)
-                    println(blue)
-                    System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
-                }
-            }
-
-
-
-//            Raster raster= image.getData();
-//            int[][] pixels=new int[width][height];
-//            for (int x=0;x<width;x++)
-//            {
-//                for(int y=0;y<height;y++)
-//                {
-//                    pixels[x][y]=raster.getSample(x,y,0);
+//            int k=1;
+//            for (int i = 0; i < height; i++) {
+//                for (int j = 0; j < width; j++) {
+//                    System.out.println("x,y: " + j + ", " + i);
+//                    pixel = image.getRGB(j, i);
+//                    k++;
+//                    println(k)
 //
-//
+//                    System.out.println("");
+//                    int alpha = (pixel >> 24) & 0xff;
+//                    int red = (pixel >> 16) & 0xff;
+//                    int green = (pixel >> 8) & 0xff;
+//                    int blue = (pixel) & 0xff;
+//                    println(alpha)
+//                    println(red)
+//                    println(green)
+//                    println(blue)
+//                    System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
 //                }
 //            }
-//            print(pixels)
-//
-//
+
+
+
+            Raster raster= image.getData();
+            int[][] pixels=new int[width][height];
+            for (int x=0;x<width;x++)
+            {
+                for(int y=0;y<height;y++)
+                {
+                    pixels[x][y]=raster.getSample(x,y,0);
+
+
+                }
+            }
+            print(pixels)
+
+
 //            return pixels;
+            redirect(action: XOR_Key() )
 
 
-          //  write it to byte array in-memory (jpg format)
+           // write it to byte array in-memory (jpg format)
 //            ByteArrayOutputStream b = new ByteArrayOutputStream();
 //            ImageIO.write(image, "png", b);
 //
@@ -121,6 +122,40 @@ class EncryptionController {
 
         }
 
+    }
+     def XOR_Key(){
+         println("XOR----")
+         String [][] key = new  String[4][4][3];
+         String [][] Plaintext = new String[4][4][3];
+         String[][] array_afterroundkey= new String[4][4][3];
+         Plaintext=[["54", "77", "6F", "20"],[ "4F", "6E", "65", "20"],[ "4E", "69", "6E", "65"],[ "20", "54", "77", "6F"]];
+
+         key =[["54", "73", "20", "67"], ["68","20", "4B", "20", ],["61", "6D", "75", "46"],[ "74", "79", "6E", "75"]];
+         println(key[0][0])
+
+         println("KEY MATRIX");
+         for (int i=0; i<4; i++){
+             for (int j=0; j<4; j++){
+                 print(key[i][j]);
+             }
+             println();
+         }
+         println("AFTER ADD ROUND KEY");
+         println(Integer.valueOf((Plaintext[1][1][1]).toLowerCase(), 16).intValue());
+         println(12^12);
+
+         println( key[0][0][0]);
+         for (int i=0; i<4; i++){
+             for (int j=0; j<4; j++){
+                 println((Integer.valueOf((Plaintext[i][j]).toLowerCase(), 16).intValue()));
+                 println((Integer.valueOf((key[i][j]).toLowerCase(), 16).intValue()) );
+                 array_afterroundkey[i][j]=Integer.toHexString( (Integer.valueOf((Plaintext[i][j]).toLowerCase(), 16).intValue())^(Integer.valueOf((key[i][j]).toLowerCase(), 16).intValue()) );
+                // array_afterroundkey[i][j][1]=Integer.valueOf((Plaintext[i][j][1]).toLowerCase(), 16).intValue()^Integer.valueOf((key[i][j]).toLowerCase(), 16).intValue();
+                // array_afterroundkey[i][j][2]=0;
+                 printf( array_afterroundkey[i][j]);
+             }
+             printf("\n");
+         }
     }
 
 
