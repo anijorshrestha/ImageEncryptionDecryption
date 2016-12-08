e376f907f0399e72b2dc9cd1230ec305ebe406a2
Sushant Gurung <tag35.sgurung@gmail.com>
2016-12-08 08:26:59 +0545

    xor

--- grails-app/controllers/encryption/EncryptionController.groovy
+++ grails-app/controllers/encryption/EncryptionController.groovy
@@ -1,28 +1,118 @@
 package encryption
-
 import grails.util.Holders
 import org.apache.commons.io.FilenameUtils
 
 import javax.imageio.ImageIO
-import java.awt.Color
 import java.awt.image.BufferedImage
-import java.awt.image.DataBufferByte
 import java.awt.image.Raster
-import java.nio.charset.StandardCharsets
-
 
 class EncryptionController {
 
-    def index() {
-        render(view: "test")
+    def index() {}
+
+    def save() {
+        savePhotoToDisk(request.getFile('photo'))
+        print("Hereee----")
 
 
     }
-    def encrypt(){
+
+    def savePhotoToDisk(def f)
+
+    {
+        println("Now heree----")
+        Constants constants = new Constants();
+        constants.PHOTOS_DIR = Holders.getGrailsApplication().getMainContext().getResource("/").getFile().getAbsolutePath();
+        println "------->" + constants.PHOTOS_DIR
+
+        def okContentTypes = ['image/png', 'image/jpeg', 'image/jpg']
+        if (f.empty) {
+            print("not empty")
+            flash.message = 'file cannot be empty'
+            return
+        }
+        if (!okContentTypes.contains(f.contentType)) {
+            print("type")
+
+            flash.message = "Image must be one of: $okContentTypes"
+            return
+        }
+        print("1------")
+        def extension = FilenameUtils.getExtension(f.originalFilename)
+
+        def serverImagesDir = new File(encryption.Constants.PHOTOS_DIR)
+        print(serverImagesDir);
+        Encryption encryption = new Encryption();
+        println("2----------")
+        if (serverImagesDir.exists()) {
+            println("photoooo---")
+            def fileName = "photo" + ".$extension"
+            File destinationFile = new File(serverImagesDir, fileName)
+
+            //  encryption.photoUrl = fileName
+
+            f.transferTo(destinationFile)
+            println("convert")
+
+            // read "any" type of image (in this case a png file)
+            BufferedImage image = ImageIO.read(new File("D:\\ImageEncryptionAndDecryption\\web-app\\photo.PNG"));
+            int height = image.getHeight();
+            int width = image.getWidth();
+            int pixel;
+//            int k=1;
+//            for (int i = 0; i < height; i++) {
+//                for (int j = 0; j < width; j++) {
+//                    System.out.println("x,y: " + j + ", " + i);
+//                    pixel = image.getRGB(j, i);
+//                    k++;
+//                    println(k)
+//
+//                    System.out.println("");
+//                    int alpha = (pixel >> 24) & 0xff;
+//                    int red = (pixel >> 16) & 0xff;
+//                    int green = (pixel >> 8) & 0xff;
+//                    int blue = (pixel) & 0xff;
+//                    println(alpha)
+//                    println(red)
+//                    println(green)
+//                    println(blue)
+//                    System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
+//                }
+//            }
 
 
+            Raster raster = image.getData();
+            int[][] pixels = new int[width][height];
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    pixels[x][y] = raster.getSample(x, y, 0);
 
 
+                }
+            }
+            print(pixels)
+
+//            return pixels;
+            redirect(action: XOR_Key())
+
+            // write it to byte array in-memory (jpg format)
+//            ByteArrayOutputStream b = new ByteArrayOutputStream();
+//            ImageIO.write(image, "png", b);
+//
+//            // do whatever with the array...
+//            byte[] jpgByteArray = b.toByteArray();
+//            println(jpgByteArray)
+//            // convert it to a String with 0s and 1s
+//            StringBuilder sb = new StringBuilder();
+//
+//            for (byte by : jpgByteArray)
+//                sb.append(Integer.toBinaryString(by & 0xFF));
+//
+//
+//            System.out.println(sb.toString());
+
+
+        }
 
     }
 
@@ -40,7 +130,7 @@ class EncryptionController {
             for (int j=0; j<4; j++){
                 print(key[i][j]);
             }
-            println("----------");
+            println();
         }
         println("AFTER ADD ROUND KEY");
         for (int i=0; i<4; i++){
@@ -53,14 +143,13 @@ class EncryptionController {
                 }
                 printf( array_afterroundkey[i][j]);
             }
-            printf("---------");
+            printf("\n");
         }
         substituteBytes(array_afterroundkey)
     }
 
     def substituteBytes(stateArray)
     {
-        println("-----------------")
         println("subbyte")
         for(int i=0;i<4;i++){
             for(int j=0;j<4;j++){
@@ -70,7 +159,7 @@ class EncryptionController {
         }
         String [][] s_box = new  String[16][16];
         String [][] substituteArray= new String[4][4];
-       // String [][] stateArray= new String[5][5][3];
+        // String [][] stateArray= new String[5][5][3];
         s_box=[["63", "7C", "77", "7B", "F2","6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76"],
                ["CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C","A4", "72", "C0"],
                ["B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15"],
@@ -94,8 +183,8 @@ class EncryptionController {
                 values2 = Integer.parseInt(stateArray[i][j].toString().charAt(1).toString().toLowerCase(),16);
                 substituteArray[i][j] = s_box[values1][values2];
 //                substituteArray[i][j]=s_box[Integer.parseInt((stateArray[i][j][0]), 16)][(Integer.parseInt((stateArray[i][j][1]), 16))];
-               // substituteArray[i][j][1]=s_box[(Integer.parseInt((stateArray[i][j][0]).toLowerCase(), 16))][(Integer.parseInt((stateArray[i][j][1]).toLowerCase(), 16))];
-              //  substituteArray[i][j][2]=NULL;
+                // substituteArray[i][j][1]=s_box[(Integer.parseInt((stateArray[i][j][0]).toLowerCase(), 16))][(Integer.parseInt((stateArray[i][j][1]).toLowerCase(), 16))];
+                //  substituteArray[i][j][2]=NULL;
 //                println "s = "  + substituteArray[i][j]
             }
 //            println("---");
@@ -104,7 +193,6 @@ class EncryptionController {
     }
 
     def shiftRow(substituteArray){
-        println("ShiftRow-------")
         String[][] shiftRows = new String [4][4];
         for(int i;i<4;i++){
             for(int j; j<4;j++){
@@ -118,7 +206,6 @@ class EncryptionController {
     }
 
     def mixColumn(shiftRows){
-        println("MixColumn--------")
         String [][] mixColumn = new String [4][4];
         int  [][] gF = new  int[4][4];
         gF=[["2","3","1","1"],["1","2","3","1"],["1","1","2","3"],["3","1","1","2"]];
@@ -130,7 +217,6 @@ class EncryptionController {
             }
         }
         println(mixColumn)
-
 
 
     }
--- grails-app/controllers/imageencryptionanddecryption/ImageController.groovy
+++ grails-app/controllers/imageencryptionanddecryption/ImageController.groovy
@@ -24,7 +24,7 @@ class ImageController {
     def savePhotoToDisk(def f)
 
     {
-        byte[] k=new byte[16];
+        byte[] k = new byte[16];
         println("Now heree----")
         Constants constants = new Constants();
         constants.PHOTOS_DIR = Holders.getGrailsApplication().getMainContext().getResource("/").getFile().getAbsolutePath();
@@ -61,7 +61,7 @@ class ImageController {
             println("convert")
 
             // read "any" type of image (in this case a png file)
-            BufferedImage image = ImageIO.read(new File("D:\\ImageEncryptionAndDecryption\\web-app\\photo.jpg"));
+            BufferedImage image = ImageIO.read(new File(constants.PHOTOS_DIR  + "images\\photo.jpg"));
             int height = image.getHeight();
             int width = image.getWidth();
             int pixel;
@@ -77,37 +77,36 @@ class ImageController {
             byte[] b1 = new byte[0];
             println(jpgByteArray)
             println(b2);
-            for (int i = 0; i < b2.length; i++) {
-                b2[i] = b[i + 620];
-                b2= aes.encrypt(b2,k,10)
-                t=Math.round((AES.t2-AES.t1)/(1000000));
-                b1=new byte[b2.length+620];
-                for(int i=0; i<b1.length; i++) {
-                    if(i<620) b1[i]=b[i];
-                    else b1[i]=b2[i-620]; }
-                inputFile=new File(jFileChooser2.getSelectedFile()+"/encryptedimage.jpg");
-                FileOutputStream fos = new FileOutputStream(inputFile);
-                fos.write(b1);
-                fos.flush();
-                fos.close();
-                for(int i=0; i<b2.length; i++)
-                    b2[i]=b1[i+620];
-                if(jRadioButton2.isSelected()==true) b2=AES.decrypt(b2,k,4);
-                else b2=AES.decrypt(b2,k,10);
-                t=Math.round((AES.t2-AES.t1)/(1000000));
-                jTextField2.setText(String.valueOf(t)+"ms");
-                for(int i=0; i<b.length; i++) {
-                    if(i<620) b[i]=b1[i];
-                    else b[i]=b2[i-620]; }
-                inputFile=new File(jFileChooser2.getSelectedFile()+"/decryptedimage.jpg");
-                fos = new FileOutputStream(inputFile);
-                fos.write(b);
-                fos.flush();
-                fos.close();
+//            for (int i = 0; i < b2.length; i++) {
+//                b2[i] = b[i + 620];
+//                b2= aes.encrypt(b2,k,10)
+//                t=Math.round((AES.t2-AES.t1)/(1000000));
+//                b1=new byte[b2.length+620];
+//                for(int i=0; i<b1.length; i++) {
+//                    if(i<620) b1[i]=b[i];
+//                    else b1[i]=b2[i-620]; }
+//                inputFile=new File(jFileChooser2.getSelectedFile()+"/encryptedimage.jpg");
+//                FileOutputStream fos = new FileOutputStream(inputFile);
+//                fos.write(b1);
+//                fos.flush();
+//                fos.close();
+//                for(int i=0; i<b2.length; i++)
+//                    b2[i]=b1[i+620];
+//                if(jRadioButton2.isSelected()==true) b2=AES.decrypt(b2,k,4);
+//                else b2=AES.decrypt(b2,k,10);
+//                t=Math.round((AES.t2-AES.t1)/(1000000));
+//                jTextField2.setText(String.valueOf(t)+"ms");
+//                for(int i=0; i<b.length; i++) {
+//                    if(i<620) b[i]=b1[i];
+//                    else b[i]=b2[i-620]; }
+//                inputFile=new File(jFileChooser2.getSelectedFile()+"/decryptedimage.jpg");
+//                fos = new FileOutputStream(inputFile);
+//                fos.write(b);
+//                fos.flush();
+//                fos.close();
 
 
-
-            }
+//        }
 
             // println(jpgByteArray)
             // convert it to a String with 0s and 1s
