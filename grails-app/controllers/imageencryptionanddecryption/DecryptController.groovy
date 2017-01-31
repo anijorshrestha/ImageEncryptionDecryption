471a156d25375ae685416db97ef1bb875d66c64b
anijor <anijorshrestha@gmail.com>
2017-01-31 17:19:59 +0545

    xor

--- grails-app/controllers/imageencryptionanddecryption/DecryptController.groovy
+++ grails-app/controllers/imageencryptionanddecryption/DecryptController.groovy
@@ -10,8 +10,10 @@ import org.apache.commons.io.FilenameUtils
 import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 
-class ImageController {
-
+/**
+ * Created by anijor on 1/31/2017.
+ */
+class DecryptController {
     def index() {}
 
     def save() {
@@ -27,6 +29,9 @@ class ImageController {
         println("Now heree----")
         Constants constants = new Constants();
         constants.PHOTOS_DIR = Holders.getGrailsApplication().getMainContext().getResource("/").getFile().getAbsolutePath();
+        println f.originalFilename
+        println f.originalFilename.split("-",2)[1].toString().split("\\.",2)[0]
+        int length = f.originalFilename.split("-",2)[1].toString().split("\\.",2)[0].toString().toInteger();
 
         def okContentTypes = ['image/png', 'image/jpeg', 'image/jpg']
         if (f.empty) {
@@ -63,42 +68,42 @@ class ImageController {
             byte[] k=new byte[16];
             try
             {
-                k=AES.keygeneration();
+                k=[-41, -28, 96, -23, -2, -72, 4, 7, -2, 109, 87, 38, 35, 46, 88, -72];
             }
             catch (Exception e){
                 println "Problem in keyGeneration!!!!!!!!";
             }
 
 //            BufferedImage image = ImageIO.read(new File("C:\\Java_Projects\\AESAlgorithm\\web-app\\images\\springsource.PNG"));
-            BufferedImage image = ImageIO.read(new File("C:\\Users\\anijor\\Desktop\\B2DBy.jpg"));
-//            BufferedImage image = ImageIO.read(new File("C:\\Users\\anijor\\Desktop\\mine.jpg"));
+            BufferedImage image = ImageIO.read(new File("C:\\Users\\anijor\\Desktop\\"+f.originalFilename));
             String path = "C:\\Users\\anijor\\Desktop\\"
             ByteArrayOutputStream baos=new ByteArrayOutputStream();
             ImageIO.write(image, "jpg", baos);
-            byte[] b = baos.toByteArray();
-            byte[] b2 = new byte[b.length-620];
-            byte[] b1 = new byte[0];
+            byte[] b1 = new byte[length];
+            b1 = baos.toByteArray();
+            byte[] b2 = new byte[b1.length-620];
+            byte[] b = new byte[b2.length+620];
 
-            for(int i=0; i<(b2.length); i++)
-                b2[i]=b[i+620];
-
-            b2=AES.encrypt(b2,k,10)
-            b1=new byte[b2.length+620];
-            for(int i=0; i<b1.length; i++) {
-                if(i<620) b1[i]=b[i];
-                else b1[i]=b2[i-620]; }
-
+//            for(int i=0; i<(b2.length); i++)
+//                b2[i]=b[i+620];
+//
+//            b2=AES.encrypt(b2,k,10)
+//            b1=new byte[b2.length+620];
+//            for(int i=0; i<b1.length; i++) {
+//                if(i<620) b1[i]=b[i];
+//                else b1[i]=b2[i-620]; }
+//
             println "Before"
             println "key == "+k
             println "b------------------->" + b.length
             println "b1------------------>" + b1.length
             println "b2------------------>" + b2.length
-            File inputFile
-            inputFile=new File(path+"encryptedimage-"+b.length+".jpg");
-            FileOutputStream fos = new FileOutputStream(inputFile);
-            fos.write(b1);
-            fos.flush();
-            fos.close();
+//            File inputFile
+//            inputFile=new File(path+"encryptedimage.jpg");
+//            FileOutputStream fos = new FileOutputStream(inputFile);
+//            fos.write(b1);
+//            fos.flush();
+//            fos.close();
             println "After encryption"
             println "b------------------->" + b.length
             println "b1------------------>" + b1.length
@@ -110,10 +115,11 @@ class ImageController {
             b2=AES.decrypt(b2,k,10)
             for(int i=0; i<b.length; i++) {
                 if(i<620) b[i]=b1[i];
-                else b[i]=b2[i-620]; }
+                else b[i]=b2[i-620];
+            }
 
-            inputFile=new File(path+"decryptedimage.jpg");
-            fos = new FileOutputStream(inputFile);
+            File inputFile=new File(path+"decrypted123123123image.jpg");
+            FileOutputStream fos = new FileOutputStream(inputFile);
             println "after decrypt"
             println "b------------------->" + b.length
             println "b1------------------>" + b1.length
